5f465f950c260e2b0e7b1f3e848cad61fada0a7b
Ryan Weaver <ryan@thatsquality.com>
2010-04-04 15:11:44 -0500

    [Upgrading] Ran the sympal install, some files were generated, regenerated, or updated.

---
+++ lib/form/doctrine/sfSympalContentListPlugin/sfSympalContentListTranslationForm.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * sfSympalContentListTranslation form.
+ *
+ * @package    sympalweb
+ * @subpackage form
+ * @author     Your name here
+ * @version    SVN: $Id: sfDoctrinePluginFormTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class sfSympalContentListTranslationForm extends PluginsfSympalContentListTranslationForm
+{
+  public function configure()
+  {
+  }
+}
