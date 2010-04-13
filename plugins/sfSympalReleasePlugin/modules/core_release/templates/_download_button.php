5bac1b86c1b182b5002d8d02753116454fbb0ffe
weaverryan <ryan@thatsquality.com>
2010-04-13 06:32:13 -0500

    Adding some basic styling for the release page by bringing in its own custom css

--- plugins/sfSympalReleasePlugin/modules/core_release/templates/_download_button.php
+++ plugins/sfSympalReleasePlugin/modules/core_release/templates/_download_button.php
@@ -1,1 +1,1 @@
-<a href="http://github.com/downloads/sympal/sympal/<?php echo $value ?>">Download <?php echo $value ?></a>
+<a class="button" href="http://github.com/downloads/sympal/sympal/<?php echo $value ?>">Download <?php echo $value ?></a>
--- plugins/sfSympalReleasePlugin/modules/core_release/templates/_view.php
+++ plugins/sfSympalReleasePlugin/modules/core_release/templates/_view.php
@@ -1,7 +1,9 @@
+<?php use_stylesheet('/sfSympalReleasePlugin/css/release.css') ?>
+
 <h1><?php echo get_sympal_content_slot('title') ?></h1>
 
 <?php echo get_sympal_content_slot('archive_filename', array('template' => 'core_release/download_button')) ?>
 
-<div>
+<div class="release_body">
   <?php echo get_sympal_content_slot('details') ?>
 </div>
