81d5b1c0073b8c415a8b1d9338625bae0921d0a8
weaverryan <ryan@thatsquality.com>
2010-05-11 05:13:27 -0500

    Modifying the Release to have the needed markup for the download button

--- plugins/sfSympalReleasePlugin/modules/core_release/templates/_download_button.php
+++ plugins/sfSympalReleasePlugin/modules/core_release/templates/_download_button.php
@@ -1,1 +1,3 @@
-<a class="button" href="http://github.com/downloads/sympal/sympal/<?php echo $value ?>">Download <?php echo $value ?></a>
+<a class="button" href="http://github.com/downloads/sympal/sympal/<?php echo $value ?>">
+  <span class="download">Download <?php echo $value ?></span>
+</a>
