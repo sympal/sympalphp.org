dcc6cb82033dd744110191c6b04d73fd31c9ad50
weaverryan <ryan@thatsquality.com>
2010-05-11 05:23:56 -0500

    Moving the template slot option from the template to the config.
    If it's in the template, then if we want to render that slot elsewhere (like when hitting cancel after editing it), we must have it in the config. There's no way to know that the slot should be rendered in the template if we're not rendering the full template itself unless it's in the config instead.

--- plugins/sfSympalReleasePlugin/modules/core_release/templates/_view.php
+++ plugins/sfSympalReleasePlugin/modules/core_release/templates/_view.php
@@ -2,7 +2,7 @@
 
 <h1><?php echo get_sympal_content_slot('title') ?></h1>
 
-<?php echo get_sympal_content_slot('archive_filename', array('template' => 'core_release/download_button')) ?>
+<?php echo get_sympal_content_slot('archive_filename') ?>
 
 <div class="release_body">
   <?php echo get_sympal_content_slot('details') ?>
